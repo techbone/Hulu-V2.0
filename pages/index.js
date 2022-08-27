ae0cc67df10e9af8571680f1298a9f5e94f1a6aa
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-08-27 22:38:47 +0100

    fix: made my code more readable

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ results }) {
       {/* Header */}
       <Header />
 
-      {/* Nav */}
+      {/* Navigation */}
       <Nav />
 
       {/* Results */}
---
+++ pages/musa.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Musa() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-blue-600">
+      Musa Muhammad Etudaye
+      <button>Musa</button>
+    </div>
+  );
+}
+
+export default Musa;
